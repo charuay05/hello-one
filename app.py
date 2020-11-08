8c41e2439153857d3066e3dacf60da3d20632f90
charuay05 <57581355+charuay05@users.noreply.github.com>
2020-11-09 00:08:00 +0700

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def elapsed():
 
 @app.route('/')
 def root():
-    return "Hello World One - 8 nov 2020 4:32 (Python)! (up %s)\n" % elapsed()
+    return "Hello World One - 9 nov 2020 00:07 (Python)! (up %s)\n" % elapsed()
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=8080)
